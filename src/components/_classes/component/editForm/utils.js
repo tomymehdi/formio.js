ec7eb6d9b457054d1bd403bf36b50a985996091e
Tomas Mehdi <tomymehdi@gmail.com>
2024-05-14 16:56:27 +0200

    test custom build

--- src/components/_classes/component/editForm/utils.js
+++ src/components/_classes/component/editForm/utils.js
@@ -55,6 +55,7 @@ const EditFormUtils = {
       '<tr><th>_</th><td>An instance of <a href="https://lodash.com/docs/" target="_blank" rel="noopener noreferrer">Lodash</a>.</td></tr>' +
       '<tr><th>utils</th><td>An instance of the <a href="http://formio.github.io/formio.js/docs/identifiers.html#utils" target="_blank" rel="noopener noreferrer">FormioUtils</a> object.</td></tr>' +
       '<tr><th>util</th><td>An alias for "utils".</td></tr>' +
+      '<tr><th>oeContext</th><td>OE Context variable.</td></tr>' +
       '</table><br/>'
       /* eslint-enable prefer-template */
     };
